df03341e18a3a60f9f4e042383033c78d87d563f
Alejandro Páez <alepdlb@gmail.com>
2024-07-16 14:02:00 +0200

    chore: add eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: false,
+  endOfLine: "auto",
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+};
