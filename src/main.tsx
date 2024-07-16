d22c4d101963c1def7055b4f12410ad3c1ef0db3
Alejandro Páez <alepdlb@gmail.com>
2024-07-16 15:22:39 +0200

    chore: change root component

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
-export default function App() {
+export default function Main() {
   return (
     <View className="flex-1 bg-white items-center justify-center">
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -9,3 +10,5 @@ export default function App() {
     </View>
   );
 }
+
+registerRootComponent(Main);
