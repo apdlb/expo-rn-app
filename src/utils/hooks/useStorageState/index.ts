3df7a9981dcf7ee690ac723ae85d79518bd73715
Alejandro Páez <alepdlb@gmail.com>
2024-08-13 17:31:24 +0200

    feat: create new hook to manage storage

---
+++ src/utils/hooks/useStorageState/index.ts
@@ -0,0 +1,74 @@
+import * as SecureStore from 'expo-secure-store';
+import * as React from 'react';
+import { Platform } from 'react-native';
+import { UseStateHook } from './types';
+
+const useAsyncState = <T>(
+  initialValue: [boolean, T | null] = [true, null],
+): UseStateHook<T> => {
+  return React.useReducer(
+    (
+      state: [boolean, T | null],
+      action: T | null = null,
+    ): [boolean, T | null] => [false, action],
+    initialValue,
+  ) as UseStateHook<T>;
+};
+
+export const setStorageItemAsync = async (
+  key: string,
+  value: string | null,
+) => {
+  if (Platform.OS === 'web') {
+    try {
+      if (value === null) {
+        localStorage.removeItem(key);
+      } else {
+        localStorage.setItem(key, value);
+      }
+    } catch (e) {
+      console.error('Local storage is unavailable:', e);
+    }
+  } else {
+    if (value == null) {
+      await SecureStore.deleteItemAsync(key);
+    } else {
+      await SecureStore.setItemAsync(key, value);
+    }
+  }
+};
+
+export const useStorageState = (key: string): UseStateHook<string> => {
+  // Public
+  const [state, setState] = useAsyncState<string>();
+
+  // Get
+  React.useEffect(() => {
+    if (Platform.OS === 'web') {
+      try {
+        if (typeof localStorage !== 'undefined') {
+          setState(localStorage.getItem(key));
+        }
+      } catch (e) {
+        console.error('Local storage is unavailable:', e);
+      }
+    } else {
+      SecureStore.getItemAsync(key).then((value) => {
+        setState(value);
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [key]);
+
+  // Set
+  const setValue = React.useCallback(
+    (value: string | null) => {
+      setState(value);
+      setStorageItemAsync(key, value);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [key],
+  );
+
+  return [state, setValue];
+};
