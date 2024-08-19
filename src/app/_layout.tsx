77bfd25cce97519aed05bb8b64043a46db3f359d
Alejandro Páez <alepdlb@gmail.com>
2024-08-19 15:35:38 +0200

    feat: add full width

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,16 @@
+import { cn } from '@/utils';
 import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
+import { NativeWindStyleSheet } from 'nativewind';
+
+NativeWindStyleSheet.setOutput({
+  default: 'native',
+});
 
 export default function HomeLayout() {
   return (
-    <View className="flex-1 bg-white items-center justify-center">
+    <View className={cn('flex-1 w-full bg-white items-center justify-center')}>
       <StatusBar style="auto" />
 
       <Slot />
--- src/pages/Root/index.tsx
+++ src/pages/Root/index.tsx
@@ -1,12 +1,16 @@
+import { TextInput } from '@/components';
+import { cn } from '@/utils';
 import { Link } from 'expo-router';
 import { Text, View } from 'react-native';
 
 export const RootPage = () => {
   return (
-    <View>
+    <View className={cn('w-full')}>
       <Text>Sign up page</Text>
 
-      <Link href={'/sign-in'}>
+      <TextInput label="Label" />
+
+      <Link href="/sign-in">
         <Text>Sign in</Text>
       </Link>
     </View>
